d0f7eea6b6d43c80a5ee141dea99b44b1f1e4926
🐢D A Y N L I G H T🐢 <danielstodulski000@gmail.com>
2024-06-28 23:51:40 +0200

    Rename css

--- src/index.php
+++ src/index.php
@@ -45,7 +45,7 @@ $lastsite_index = count($sites) - 1;
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>LinkFree Generator</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="preview.css">
+  <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
